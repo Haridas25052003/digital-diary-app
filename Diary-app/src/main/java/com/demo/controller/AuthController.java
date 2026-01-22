c8f515104455d5a60203ac32b18a9fbd329dd0d4
Haridas250503 <haridasshinde314@gmail.com>
2026-01-22 23:27:00 +0530

    completed all the controllers and service layers

--- Diary-app/src/main/java/com/demo/controller/AuthController.java
+++ Diary-app/src/main/java/com/demo/controller/AuthController.java
@@ -3,36 +3,35 @@ package com.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.demo.model.User;
 import com.demo.service.UserService;
-import com.demo.model.*;
 
 @RestController
+@RequestMapping("/api/auth")   
 public class AuthController {
-	
-	@Autowired
-	private UserService us;
-	
-	@GetMapping(value="/")
-	public List<User> m1(){
-		return us.findAll();
-	}
-	
-	@GetMapping(value="/req1/{email}")
-	public List<User> m2(@PathVariable String email){
-		return us.findByEmail(email);
-	}
 
-	@GetMapping(value="req2/{id}")
-	public List<User> m3(@PathVariable int id){
-		return us.findById(id);
-	}
-	
-	@GetMapping(value="req3/{name}")
-	public List<User> m4(@PathVariable String name){
-		return us.findByName(name);
-	}
+    @Autowired
+    private UserService us;
+
+    @GetMapping("/all")
+    public List<User> getAllUsers() {
+        return us.findAll();
+    }
+
+    @GetMapping("/req1/{email}")
+    public List<User> getUserByEmail(@PathVariable String email) {
+        return us.findByEmail(email);
+    }
+
+    @GetMapping("/req2/{id}")
+    public List<User> getUserById(@PathVariable int id) {
+        return us.findById(id);
+    }
+
+    @GetMapping("/req3/{name}")
+    public List<User> getUsersByName(@PathVariable String name) {
+        return us.findByName(name);
+    }
 }
--- Diary-app/src/main/java/com/demo/controller/DiaryController.java
+++ Diary-app/src/main/java/com/demo/controller/DiaryController.java
@@ -3,34 +3,41 @@ package com.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.demo.dao.DiaryDao;
 import com.demo.model.Diary;
 import com.demo.service.DiaryService;
 
 @RestController
+@RequestMapping("/api/diary")   // important to avoid mapping conflict
 public class DiaryController {
 
-	@Autowired
-	private DiaryService ds;
-	
-	@GetMapping(value="/dreq1")
-	public List<Diary> m1(){
-		return ds.findAll();
-	}
-	
-	@GetMapping(value="/dreq2/{userId}")
-	public List<Diary> m2(@PathVariable int userId){
-		return ds.findByUserId(userId);
-	}
-	
-	@GetMapping(value="/dreq3/{id}")
-	public List<Diary> m3(@PathVariable int id){
-		return ds.findById(id);
-	}
-	
-	
+    @Autowired
+    private DiaryService ds;
+
+    @GetMapping("/all")
+    public List<Diary> getAllDiaries() {
+        return ds.findAll();
+    }
+
+    @GetMapping("/dreq/{userId}")
+    public List<Diary> getDiariesByUserId(@PathVariable int userId) {
+        return ds.findByUserId(userId);
+    }
+ 
+    @GetMapping("/dreq1/{id}")
+    public List<Diary> getDiaryById(@PathVariable int id) {
+        return ds.findById(id);
+    }
+
+    @PostMapping("/dreq2/save")
+    public Diary saveDiary(@RequestBody Diary diary) {
+        return ds.saveDiary(diary);
+    }
+    
+    @DeleteMapping("/dreq3/{id}")
+    public String deleteDiary(@PathVariable int id) {
+        ds.deleteDiary(id);
+        return "Diary deleted successfully";
+    }
 }
