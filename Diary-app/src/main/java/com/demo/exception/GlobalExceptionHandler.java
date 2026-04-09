4cba0d45b4b33968a0d2e1a823fc93af5354388a
Haridas Shinde <haridasshinde314@gmail.com>
2026-04-10 00:11:10 +0530

    minor changes

--- Diary-app/src/main/java/com/demo/exception/GlobalExceptionHandler.java
+++ Diary-app/src/main/java/com/demo/exception/GlobalExceptionHandler.java
@@ -4,11 +4,10 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.coyote.BadRequestException;
+import com.demo.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
